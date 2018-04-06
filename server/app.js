89dd64bbdb505eac2b6d1398d516f508886234ee
belodpav <belod.pav@gmail.com>
2018-04-06 20:39:16 +0300

    (config) Настраивает статику

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const SuffixTrie = require('./suffixTrie');
-
+const path = require('path');
 const express = require('express');
 const app = express();
 
 const trie = new SuffixTrie();
+
+app.use('/', express.static(path.resolve('.', 'client/dist/')));
 
 app.get('/search/?', (req, res) => {
     const reqText = req.query.text;
@@ -13,7 +15,9 @@ app.get('/search/?', (req, res) => {
     res.send(JSON.stringify(result));
 });
 
-app.use('/', express.static('./client/dist'));
+app.get('/', (req, res) => {
+    res.sendFile('index.html', {root: path.resolve('.', 'client/dist/')});
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running ...');
