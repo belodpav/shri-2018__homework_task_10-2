5098afaad9af7c1447490e2666d2df1880887646
belodpav <belod.pav@gmail.com>
2018-04-06 20:18:20 +0300

    experiments...

--- server/app.js
+++ server/app.js
@@ -1,24 +1,19 @@
-// const SuffixTrie = require('./suffixTrie');
+const SuffixTrie = require('./suffixTrie');
 
 const express = require('express');
 const app = express();
 
-// const trie = new SuffixTrie();
+const trie = new SuffixTrie();
 
-// app.use('/', express.static('./client/dist'));
+app.get('/search/?', (req, res) => {
+    const reqText = req.query.text;
+    const result = trie.find(reqText);
 
-
-// app.get('/search/?', (req, res) => {
-//     const reqText = req.query.text;
-//     const result = trie.find(reqText);
-
-//     res.header('Content-Type', 'application/json');
-//     res.send(JSON.stringify(result));
-// });
-
-app.get('/', (req, res) => {
-    res.send('test');
+    res.header('Content-Type', 'application/json');
+    res.send(JSON.stringify(result));
 });
+
+app.use('/', express.static('./client/dist'));
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running ...');
